7a3f45b131ebb95d6747d6842ce65939754b1e33
Jphsx <justin.anguiano1@gmail.com>
2014-09-30 13:08:39 -0500

    more fleshed out program architecure, mainly pseduocode

--- quash.c
+++ quash.c
@@ -11,11 +11,37 @@ int parseEntry(char *entry){
 	//add specific functions to be executed (like & or pipes),
 	// otherwise execl with
 	// command parameters loaded into buffer
-	
+	else if(entry contains | or >){
+	//pipe outputs
+		pipeLink(entry);
+	}
+	else if(entry contains &){
+	//use a special fork method, and pass in entry
+		runBackground(entry);
+	}
+	else{
+		execl(entry,(char*)0);
+	}
+	return 0;
+}
+void pipeLink(char *entry){
+	//scan for | char in entry, for N pipes, fork N+1 processes
+	//declare N pipes
+	//connect processes
+	//
+}
+void runBackground(char *entry){
+	// assign a JOBID[PID] to the background process
+	printf(JOBID[PID] running in backgound);
+	entry = entry - &;
+	//remove the & from entry and pass entry back into parseEntry
+	parseEntry(entry);
+	//parse entry will execute normally 
+	printf(JOBID[PID] finished COMMAND);
 	
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv,char **envp)
 {
 char entry[100];
 
