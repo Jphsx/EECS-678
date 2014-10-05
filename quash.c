03d8b8454f144333b811fdb680df598b4a3997e2
Jphsx <justin.anguiano1@gmail.com>
2014-10-05 00:28:25 -0500

    working background process implementation, exit, and job printing

--- quash.c
+++ quash.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 struct job {
 	//job flag is 1 for process in progress, 0 for finished
@@ -13,83 +14,109 @@ struct job {
 };
 //global jobs array
 struct job joblist[100];
+int globalJobCounter=0;
 
+char* removeChar(char*string, char character){
+	int i=0;
+	while(i<strlen(string)-1){
+		if(string[i]==character){
+			string[i]='\0';
+		}
+	i=i+1;
+	}
+	return string;
+}
 
+//return 0 if a character is in a string, and 1 if it is not
+int containsChar(char* string, char character){
+	int i=0;
+	while(i<strlen(string)-1){
+		if(string[i]==character){
+			return 0;
+		}
+	i=i+1;
+	}
+	return 1;
+}
+//takes a task and splits off a child process to execute it
+//stores the job information in the struct job array
+//for later access
 void runBackgroundTask(char* task){
-  pid_t childpid;
-       //put the background process in the array element of the parent ID
+	  pid_t childpid;
+	
+	if(globalJobCounter == 99){
+		globalJobCounter = 0;
+	}
+// store the counter incase it is changed during execution, increment
+	int counterCopy = globalJobCounter;
+	globalJobCounter++;
+
+	int childReturnValue=0;
+
 	childpid = fork();
 	if(childpid==0){
-		joblist[getppid()].jobFlag=1;
-		printf("[%d] %d running in background %s",getppid(),getpid(),task);
-		joblist[getppid()].JOBID=getppid();
-		joblist[getppid()].PID= getpid();
-		strcpy(joblist[getppid()].COMMAND, task);
-		parseEntry(task);
+		
+		printf("[%d] %d running in background %s \n", counterCopy,getppid(),task);
+		//pass task back to parse for execution
+		dummyJob();
+		
+		printf("[%d] %d finished in background %s \n", counterCopy,getppid(),task);
+		joblist[counterCopy].jobFlag=0;
 	}
 	else{
-		printf("[%d] %d finished %s", joblist[getpid()].JOBID,joblist[getpid()].PID,joblist[getpid()].COMMAND);
-		joblist[getpid()].jobFlag=0;
+		//parent process populates job struct and pushes onto job stack
+		struct job childJob;
+		childJob.jobFlag = 1;
+		childJob.JOBID = counterCopy;	
+		childJob.PID = getpid();
+		strcpy(childJob.COMMAND, task);
+		joblist[counterCopy] = childJob;
+	}	
+}
+//iterates through the job stack and checks 
+//for any job in progress flag then prints
+//out all the information for each background job
+void printJobList(){
+	int i=0;
+	while( i<99) {
+		if(joblist[i].jobFlag == 1){
+			printf("[%d] %d running in background %s",joblist[i].JOBID,joblist[i].PID,joblist[i].COMMAND);
+		}
+		i=i+1;
 	}
 	
+}
+void dummyJob(){
+	int k=5;
+	while(k==5){};
 }
  
 int parseEntry(char*  entry){
 	//detect exit
 	 char quit[5];
-	//put exit into exit char array to compare to entry
 	strcpy(quit,"exit\n");
-	
 	if(strcoll(quit, entry)==0){
 		printf("\n");
 		exit(0);
 	}
 	
 	// check for & to run in background
-	char background[1];
-	char modifiedEntry[100];
-	strcpy(background, "&");
-	
-	if(strpbrk(background, entry) != NULL){
-		//remove the &, fork and re parse
-	//	strcpy(modifiedEntry,entry,strlen(entry)-1);
-		runBackgroundTask(modifiedEntry);	
-
+	if(containsChar(entry,'&')==0){	
+	runBackgroundTask(removeChar(entry));
 	}
 
-/*	//add specific functions to be executed (like & or pipes),
-	// otherwise execl with
-	// command parameters loaded into buffer
-	else if(entry contains | or >){
-	//pipe outputs
-		pipeLink(entry);
+	//check for jobs to print to screen
+	char jobCommand[5];
+	strcpy(jobCommand,"jobs\n");
+	if(strcoll(jobCommand,entry)==0){
+		printJobList();
 	}
-	else if(entry contains &){
-	//use a special fork method, and pass in entry
-		runBackground(entry);
-	}
-	else{
-		execl(entry,(char*)0);
-	}
-	return 1;
-*/
 }
 void pipeLink(char *entry){
 	//scan for | char in entry, for N pipes, fork N+1 processes
 	//declare N pipes
 	//connect processes
 	//
-}
-void runBackground(char *entry){
-/*	// assign a JOBID[PID] to the background process
-	printf(JOBID[PID] running in backgound);
-	entry = entry - &;
-	//remove the & from entry and pass entry back into parseEntry
-	parseEntry(entry);
-	//parse entry will execute normally 
-	//when finished print to screen
-	printf(JOBID[PID] finished COMMAND);
-*/	
 }
 
 int main(int argc, char **argv,char **envp)
